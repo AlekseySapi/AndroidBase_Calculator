24baedbbdd459c800c11db9c3cdbb4803c7a3ead
Aleksey L <52339899+AlekseySapi@users.noreply.github.com>
2021-06-14 15:15:29 +0300

    Изменил главный макет, запретил смену ориентации

--- app/src/main/java/ru/geekbrains/calculator/MainActivity.java
+++ app/src/main/java/ru/geekbrains/calculator/MainActivity.java
@@ -2,6 +2,7 @@ package ru.geekbrains.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,69 @@ public class MainActivity extends AppCompatActivity {
     private String errMessage = "Ошибка";
 
     private char[] charArr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // Запретил смену ориентации экрана
+        setContentView(R.layout.activity_main);
+
+        initTextView();
+        initDigitButtons();
+        initActionButtons();
+
+
+    }
+
+    private void initTextView() {
+        operation = findViewById(R.id.operation);
+        entryField = findViewById(R.id.number_entry);
+    }
+
+    private void initDigitButtons() {
+        Button button0 = findViewById(R.id.button0);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button buttonPoint = findViewById(R.id.button_point);
+
+        button0.setOnClickListener(listenerZero);
+        button1.setOnClickListener(listenerDigits);
+        button2.setOnClickListener(listenerDigits);
+        button3.setOnClickListener(listenerDigits);
+        button4.setOnClickListener(listenerDigits);
+        button5.setOnClickListener(listenerDigits);
+        button6.setOnClickListener(listenerDigits);
+        button7.setOnClickListener(listenerDigits);
+        button8.setOnClickListener(listenerDigits);
+        button9.setOnClickListener(listenerDigits);
+        buttonPoint.setOnClickListener(listenerPoint);
+    }
+
+    private void initActionButtons() {
+        Button buttonPlus = findViewById(R.id.button_plus);
+        Button buttonMinus = findViewById(R.id.button_minus);
+        Button buttonMulti = findViewById(R.id.button_multiply);
+        Button buttonDiv = findViewById(R.id.button_divide);
+        Button buttonEquals = findViewById(R.id.button_equals);
+        Button backspace = findViewById(R.id.button_backspace);
+        Button buttonC = findViewById(R.id.button_c);
+
+        buttonPlus.setOnClickListener(listenerActions);
+        buttonMinus.setOnClickListener(listenerActions);
+        buttonMulti.setOnClickListener(listenerActions);
+        buttonDiv.setOnClickListener(listenerActions);
+        buttonEquals.setOnClickListener(listenerEquals);
+        backspace.setOnClickListener(listenerBackspace);
+        buttonC.setOnClickListener(listenerC);
+    }
+
 
     private final View.OnClickListener listenerDigits = view -> {
         Button button = (Button) view;
@@ -213,70 +277,6 @@ public class MainActivity extends AppCompatActivity {
             entryField.setText("0");
         }
     };
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        initTextView();
-        initDigitButtons();
-        initActionButtons();
-
-
-    }
-    
-
-    private void initTextView() {
-        operation = findViewById(R.id.operation);
-        entryField = findViewById(R.id.number_entry);
-    }
-
-    private void initDigitButtons() {
-        Button button0 = findViewById(R.id.button0);
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
-        Button button5 = findViewById(R.id.button5);
-        Button button6 = findViewById(R.id.button6);
-        Button button7 = findViewById(R.id.button7);
-        Button button8 = findViewById(R.id.button8);
-        Button button9 = findViewById(R.id.button9);
-        Button buttonPoint = findViewById(R.id.button_point);
-
-        button0.setOnClickListener(listenerZero);
-        button1.setOnClickListener(listenerDigits);
-        button2.setOnClickListener(listenerDigits);
-        button3.setOnClickListener(listenerDigits);
-        button4.setOnClickListener(listenerDigits);
-        button5.setOnClickListener(listenerDigits);
-        button6.setOnClickListener(listenerDigits);
-        button7.setOnClickListener(listenerDigits);
-        button8.setOnClickListener(listenerDigits);
-        button9.setOnClickListener(listenerDigits);
-        buttonPoint.setOnClickListener(listenerPoint);
-    }
-
-    private void initActionButtons() {
-        Button buttonPlus = findViewById(R.id.button_plus);
-        Button buttonMinus = findViewById(R.id.button_minus);
-        Button buttonMulti = findViewById(R.id.button_multiply);
-        Button buttonDiv = findViewById(R.id.button_divide);
-        Button buttonEquals = findViewById(R.id.button_equals);
-        Button backspace = findViewById(R.id.button_backspace);
-        Button buttonC = findViewById(R.id.button_c);
-
-        buttonPlus.setOnClickListener(listenerActions);
-        buttonMinus.setOnClickListener(listenerActions);
-        buttonMulti.setOnClickListener(listenerActions);
-        buttonDiv.setOnClickListener(listenerActions);
-        buttonEquals.setOnClickListener(listenerEquals);
-        backspace.setOnClickListener(listenerBackspace);
-        buttonC.setOnClickListener(listenerC);
-    }
 
     private void clearFields() {
         operation.setText("");
